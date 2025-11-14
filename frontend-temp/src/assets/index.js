47965a326e70bc914fff1041312a2b9585edbb29
BethTawiah <elizabeth.tawiah@amalitech.org>
2025-11-14 22:56:25 +0000

    rename frontend folder

--- frontend-temp/src/assets/index.js
+++
@@ -1,4 +0,0 @@
-// src/assets/index.js
-import heroImg from './heroImg.jpg'; 
-
-export { heroImg };
--- frontend-temp/src/config/axios.js
+++
@@ -1,9 +0,0 @@
-// src/config/axios.js
-import axios from "axios";
-
-export const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
-  headers: {
-    "Content-Type": "application/json",
-  },
-});
